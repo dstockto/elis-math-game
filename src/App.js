d34e6feee0246d891f54dae89261a01ce3771714
David Stockton <dstockton@i3logix.com>
2019-05-17 15:55:34 -0600

    Initial commit with html prototype

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Eli's Math Game</header>
+      <section className={'game'}>
+        <div className={'problem'}>345 + 291 = <input type={'text'} size={5}/></div>
+        <div className={'problem'}>222 + 555 = 777 <span role={'img'} aria-label={"Correct"}>😎</span></div>
+        <div className={'problem'}>435 + 123 = 36 <span role={'img'} aria-label={"Incorrect"}>🤬</span></div>
+      </section>
+      <section className={'scores'}>
+        <div>Correct: 1</div>
+        <div>Incorrect: 1</div>
+        <div>Remaining: 48</div>
+      </section>
+      <section className={'graphs'}>
+        Graphs coming soon!
+      </section>
     </div>
   );
 }
